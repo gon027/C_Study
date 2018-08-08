99dc7ed9f3711af20afa7575b88a51c25fc37552
Gon <gon27.tk08@gmail.com>
2018-08-08 15:26:28 +0900

    新しいファイルを追加

---
+++ cpp_study/StrLen.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// constを入れることによって実行可能
+// String型からchar型に変えたことによってエラーがでる
+
+size_t StrLen(const char* str){
+    size_t i;
+    for(i = 0; str[i] != '\0'; ++i){
+        // 何もしない
+    }
+    return i;
+}
+
+void ShowLength(const char* str) {
+    cout << "文字列『" << str << "』の長さは"
+         << StrLen(str) << "バイトです。" << endl;
+}
+
+int main(){
+    ShowLength("Hello");
+    ShowLength("");
+
+    return 0;
+}
